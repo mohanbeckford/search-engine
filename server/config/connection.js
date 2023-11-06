586ee3d21c26c31869fc62b40d3c5baa35257f42
mohanbeckford <mohanbeckford1@gmail.com>
2023-11-06 14:45:10 -0500

    updated files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mohanbeckford1:Tech263nose685%40@cluster0.6plsglq.mongodb.net');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mohanbeckford1:Tech263nose685%40@cluster0.6plsglq.mongodb.net/');
 
 module.exports = mongoose.connection;
