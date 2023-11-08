e51b29de5617b8c01a7fc4227219b72bbf901889
mohanbeckford <mohanbeckford1@gmail.com>
2023-11-07 21:19:08 -0500

    Update to server and schema

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const cookieParser = require('cookie-parser');
+const cors = require('cors'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const { typeDefs, resolvers } = require('./schema/index');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', {
+  // useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,3 +30,46 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+
+// Apollo Server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    // Implement your authentication logic here
+    let user = null;
+
+    // Check the authorization headers or cookies, depending on your authentication method
+    const token = req.headers.authorization || req.cookies.token;
+
+    // If a token is present, verify it and find the user
+    if (token) {
+      try {
+        // validate user in db
+        user = await verifyAndFindUser(token);
+      } catch (error) {
+        // Handle token verification errors
+        console.error('Token verification error:', error);
+      }
+    }
+
+    return {
+      user, 
+    };
+  },
+});
+
+// Start Apollo Server and apply middleware to Express
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`🚀 Apollo Server is now running on http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
+
+startServer();
